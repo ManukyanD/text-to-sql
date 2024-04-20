ddecc37d27b468f8ad655e89ea7b093f5558fc32
Manukyan Davit <d.manukyan6139@gmail.com>
2024-04-20 11:44:28 +0400

    fix: split dataset correctly

--- src/dataset/text2sql_dataset.py
+++ src/dataset/text2sql_dataset.py
@@ -1,5 +1,4 @@
 import json
-import os
 
 import pandas
 from torch.utils.data import Dataset
@@ -13,25 +12,26 @@ class Text2SqlDataset(Dataset):
         self.input_preprocessor = InputPreprocessor(args)
         df = pandas.read_csv(args.data_path)
         nat_sql = json.load(open(args.nat_sql_path))
+        df.insert(0, 'nat_sql', [item["NatSQL"] for item in nat_sql])
 
         # split into train and val datasets in a way that no database is shared
         total_example_count = df.shape[0]
         train_example_count = total_example_count * 0.9
         unique_db_with_example_counts = df.groupby("spider_db_name").count()
+        train_db_names = []
         count = 0
-        for i in range(unique_db_with_example_counts.shape[0]):
-            count += unique_db_with_example_counts['question'].iloc[i]
+        for i, row in unique_db_with_example_counts.iterrows():
+            count += row.question
+            train_db_names.append(row.name)
             if count >= train_example_count:
                 break
-
-        df = df.head(count) if split == "train" else df.tail(df.shape[0] - count)
-        nat_sql = nat_sql[:count] if split == "train" else nat_sql[count:]
+        df = df[df.spider_db_name.isin(train_db_names) if split == "train" else ~df.spider_db_name.isin(train_db_names)]
 
         self.dataset = []
-        for i in range(df.shape[0]):
+        for i, row in df.iterrows():
             self.dataset.append({
-                "input": self.input_preprocessor.get_input(df['question'].iloc[i], df['schema'].iloc[i]),
-                "label": self.input_preprocessor.get_label(nat_sql[i]['NatSQL'])
+                "input": self.input_preprocessor.get_input(row.question, row.schema),
+                "label": self.input_preprocessor.get_label(row.nat_sql)
             })
 
     def __getitem__(self, item):
